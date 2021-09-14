06475197282f1b57179e8438d7031ab67586908c
potato-csh <1103782026@qq.com>
2021-09-14 18:57:13 +0800

    首页广告

--- meiduo_mall/meiduo_mall/utils/fastdfs/fdfs_storage.py
+++ meiduo_mall/meiduo_mall/utils/fastdfs/fdfs_storage.py
@@ -17,8 +17,18 @@ class MyStorage(Storage):
         :param mode: 文件打开方式
         :return:None
         """
-        # 因为当前不是为了打开某个文件，所以该方法无效
+        # 因为当前不是为了打开某个文件，所以该方法无效，但是又必须重写，所以pass
         pass
 
     def _save(self, name, content):
+        """
+        保存文件会被调用：文档告诉我必须重写
+        :param name: 文件路径
+        :param content: 文件的二进制内容
+        :return: None
+        """
+        # 因为当前不是为了保存某个文件，所以该方法无效，但是又需要重写，所以pass
         pass
+
+    def
+
