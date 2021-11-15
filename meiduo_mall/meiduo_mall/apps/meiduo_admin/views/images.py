bde8a7910a92bda98a5e012e1c7108d13ce9a29b
potato-csh <1103782026@qq.com>
2021-11-15 18:49:31 +0800

    商品管理-图片管理...

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/views/images.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/views/images.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from fdfs_client.client import Fdfs_client
 
 from goods.models import SKUImage, SKU
 from meiduo_admin.serializers.images import ImageSerializer, SKUSerializer
@@ -18,4 +20,25 @@ class ImageView(ModelViewSet):
         """获取sku商品id"""
         skus = SKU.objects.all()
         ser = SKUSerializer(skus,many=True)
+        return Response(ser.data)
+
+    # 重写拓展类的保存逻辑
+    def create(self, request, *args, **kwargs):
+        # 1、获取前端数据
+        data = request.data
+        # 2、验证数据
+        ser = self.get_serializer(data=data)
+        ser.is_valid()
+        # 3、建⽴fastdfs的客户端
+        client = Fdfs_client(settings.FASTDFS_PATH)
+        file = request.FILES.get('image')
+        # 4、上传图⽚
+        res = client.upload_by_buffer(file.read())
+        # 5、判断是否上传成功
+        if res['Status'] != 'Upload successed.':
+            return Response({'error':'图片上传失败'})
+        # 6、保存图⽚表
+        img = SKUImage.objects.create(sku=ser.validated_data['sku'],image=res['Remote file_id'])
+        ser.save()
+        # 7、返回保存后的图⽚数据
         return Response(ser.data)
